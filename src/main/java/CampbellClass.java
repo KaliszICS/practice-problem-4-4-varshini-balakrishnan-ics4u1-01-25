e3744c3bd8fb836af012e5f78cd2a3c7ce30f278
Varshini Balakrishnan <varshini.balakrishnan@student.tdsb.on.ca>
2026-01-20 16:34:15 +0000

    wip

---
+++ src/main/java/CampbellClass.java
@@ -0,0 +1,30 @@
+public final class CampbellClass {
+    private CampbellClass() {
+
+    }
+    //convertString
+    public static String convertString(int num) {
+        return "" + num;
+    }
+    public static String convertString(double num) {
+        return "" + num;
+    }
+    public static String convertString(char ch) {
+        return "" + ch;
+    }
+    public static String convertString(boolean b) {
+        return "" + b;
+    }
+    //convertMetersToCenti 
+    public static int convertMetersToCenti(int meters) {
+        return meters * 100;
+    }
+    public static double convertMetersToCenti(double meters) {
+        return meters * 100;
+    }
+    //removeNonAlpha
+    public static String removeNonAlpha(String str) {
+        
+
+    }
+}
